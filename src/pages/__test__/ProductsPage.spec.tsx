110769d94673bbfa5b7ce1e6c0020912e94ba8f3
ivanexDev <ivanexdev@gmail.com>
2025-01-10 17:04:33 -0300

    Configure testing enviroment

---
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react'
+import { test } from "vitest";
+import App from "../../App";
+
+
+test("Loads and displays title",()=>{
+    render(<App/>)
+
+    screen.findAllByRole("heading", {name: "Product price updater"})
+})
