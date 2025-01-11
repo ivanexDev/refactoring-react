7c64ed9b06cb0d2d11aaa9e715f2447f9b478ee1
ivanexDev <ivanexdev@gmail.com>
2025-01-11 16:18:20 -0300

    Add pre push commands for husky

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,10 +1,9 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 import { test } from "vitest";
 import App from "../../App";
 
+test("Loads and displays title", async () => {
+    render(<App />);
 
-test("Loads and displays title",()=>{
-    render(<App/>)
-
-    screen.findAllByRole("heading", {name: "Product price updater"})
-})
+    await screen.findAllByRole("heading", { name: "Product price updater" });
+});
