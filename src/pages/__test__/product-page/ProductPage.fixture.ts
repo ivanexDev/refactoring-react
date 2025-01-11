6ee9b1ab79f3fbcbe1ef627aa44f62e0d0860e36
ivanexDev <ivanexdev@gmail.com>
2025-01-11 19:39:18 -0300

    Add test that verify when there are no data

---
+++ src/pages/__test__/product-page/ProductPage.fixture.ts
@@ -0,0 +1,23 @@
+import { MockWebServer } from "../../../tests/MockWebServer";
+import productResponse from "./data/productResponse.json"
+
+
+export function givenAProducts(mockWebServer:MockWebServer) {
+    mockWebServer.addRequestHandlers([{
+        method: "get",
+        endpoint: "https://fakestoreapi.com/products",
+        response: productResponse,
+        httpStatusCode: 200
+    }])
+
+}
+
+export function givenThereAreNoProducts(mockWebServer:MockWebServer) {
+    mockWebServer.addRequestHandlers([{
+        method: "get",
+        endpoint: "https://fakestoreapi.com/products",
+        response: [],
+        httpStatusCode: 200
+    }])
+
+}
---
+++ src/pages/__test__/product-page/ProductsPage.spec.tsx
@@ -0,0 +1,43 @@
+import { render, RenderResult, screen } from "@testing-library/react";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
+import { ProductsPage } from "../../ProductsPage";
+import { AppProvider } from "../../../context/AppProvider";
+import { ReactNode } from "react";
+import { MockWebServer } from "../../../tests/MockWebServer";
+import { givenAProducts, givenThereAreNoProducts } from "./ProductPage.fixture";
+import { verifyHeader } from "./ProductPage.helpers";
+
+const mockWebServer = new MockWebServer();
+
+describe("ProductsPage", () => {
+    beforeAll(() => mockWebServer.start());
+    afterEach(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
+
+    givenAProducts(mockWebServer);
+
+    test("Loads and displays title", async () => {
+        RenderComponent(<ProductsPage />);
+
+        await screen.findAllByRole("heading", { name: "Product price updater" });
+    });
+
+    test('should show an empty table', async() => { 
+        givenThereAreNoProducts(mockWebServer)
+
+        RenderComponent(<ProductsPage/>)
+
+        const rows = await screen.findAllByRole("row")
+
+        expect(rows.length).toBe(1)
+
+        verifyHeader(rows[0])
+
+     })
+
+
+});
+
+function RenderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
