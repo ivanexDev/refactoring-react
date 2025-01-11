bca88164395b966bfed073509a4789533901b2ff
ivanexDev <ivanexdev@gmail.com>
2025-01-11 17:02:11 -0300

    Add custom render

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,9 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, RenderResult, screen } from "@testing-library/react";
 import { test } from "vitest";
-import App from "../../App";
+import { ProductsPage } from "../ProductsPage";
+import { AppProvider } from "../../context/AppProvider";
+import { ReactNode } from "react";
 
 test("Loads and displays title", async () => {
-    render(<App />);
+    RenderComponent(<ProductsPage />);
 
     await screen.findAllByRole("heading", { name: "Product price updater" });
 });
+
+function RenderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
